2cbfc71d1589a26fe44d22dae92098db971629ac
dozer-jang <dotat.jang@gmail.com>
2023-04-12 20:37:41 +0900

    change cli_query_logs from manual test to table driven test

--- cli/logs_test.go
+++ cli/logs_test.go
@@ -17,32 +17,37 @@ func init() {
 
 func Test_logs(t *testing.T) {
 
-	var got string
-	var want string
+	tests := map[string]struct {
+		args []string
+		want string
+	}{
+		"logs --query": {
+			args: []string{"logs", "--query"},
+			want: "Error: flag needs an argument: --query\nUsage:\n  lethetool logs [flags]\n\nFlags:\n  -h, --help           help for logs\n  -q, --query string   letheql",
+		},
+		`logs --query pod{namespace=""}`: {
+			args: []string{"logs", "--query", `pod{namespace=""}`},
+			want: "namespace value cannot be empty",
+		},
+		`logs --query pod{namespace="ns-not-exists"}`: {
+			args: []string{"logs", "--query", `pod{namespace="ns-not-exists"}`},
+			want: "",
+		},
+		`logs --query pod{namespace="namespace01"}`: {
+			args: []string{"logs", "--query", `pod{namespace="namespace01"}`},
+			want: "{ 2009-11-10T21:00:00Z namespace01 nginx-deployment-75675f5897-7ci7o nginx  hello world}\n{ 2009-11-10T21:01:00Z namespace01 nginx-deployment-75675f5897-7ci7o nginx  hello world}\n{ 2009-11-10T21:02:00Z namespace01 nginx-deployment-75675f5897-7ci7o nginx  hello world}\n{ 2009-11-10T22:56:00Z namespace01 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:56:00Z namespace01 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:56:00Z namespace01 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:57:00Z namespace01 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:57:00Z namespace01 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:57:00Z namespace01 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:58:00Z namespace01 nginx-deployment-75675f5897-7ci7o sidecar  hello from sidecar}\n{ 2009-11-10T22:58:00Z namespace01 nginx-deployment-75675f5897-7ci7o sidecar  lerom from sidecar}\n{ 2009-11-10T22:58:00Z namespace01 nginx-deployment-75675f5897-7ci7o sidecar  hello from sidecar}\n{ 2009-11-10T22:59:00Z namespace01 nginx-deployment-75675f5897-7ci7o nginx  lerom ipsum}\n{ 2009-11-10T22:59:00Z namespace01 nginx-deployment-75675f5897-7ci7o nginx  hello world}",
+		},
+		`logs --query pod{namespace="namespace02"}`: {
+			args: []string{"logs", "--query", `pod{namespace="namespace02"}`},
+			want: "{ 2009-11-10T22:58:00Z namespace02 nginx-deployment-75675f5897-7ci7o nginx  hello world}\n{ 2009-11-10T22:58:00Z namespace02 nginx-deployment-75675f5897-7ci7o nginx  lerom ipsum}\n{ 2009-11-10T22:58:00Z namespace02 nginx-deployment-75675f5897-7ci7o nginx  hello world}\n{ 2009-11-10T22:58:00Z namespace02 nginx-deployment-75675f5897-7ci7o sidecar  hello from sidecar}\n{ 2009-11-10T22:58:00Z namespace02 nginx-deployment-75675f5897-7ci7o sidecar  lerom from sidecar}\n{ 2009-11-10T22:58:00Z namespace02 nginx-deployment-75675f5897-7ci7o sidecar  hello from sidecar}\n{ 2009-11-10T22:58:00Z namespace02 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:58:00Z namespace02 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:58:00Z namespace02 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:58:00Z namespace02 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:58:00Z namespace02 apache-75675f5897-7ci7o httpd  hello from sidecar}\n{ 2009-11-10T22:58:00Z namespace02 apache-75675f5897-7ci7o httpd  hello from sidecar}",
+		},
+	}
 
-	got = execute("logs")
-	want = "error: logs command needs an flag: --query"
-	assert.Equal(t, want, got)
-
-	got = execute("logs", "--query")
-	want = "Error: flag needs an argument: --query\nUsage:\n  lethetool logs [flags]\n\nFlags:\n  -h, --help           help for logs\n  -q, --query string   letheql"
-	assert.Equal(t, want, got)
-
-	got = execute("logs", "--query", `pod{namespace=""}`)
-	want = "namespace value cannot be empty"
-	assert.Equal(t, want, got)
-
-	got = execute("logs", "--query", `pod{namespace="ns-not-exists"}`)
-	want = ""
-	assert.Equal(t, want, got)
-
-	got = execute("logs", "--query", `pod{namespace="namespace01"}`)
-	want = "2009-11-10T21:00:00.000000Z[namespace01|nginx-deployment-75675f5897-7ci7o|nginx] hello world\n2009-11-10T21:01:00.000000Z[namespace01|nginx-deployment-75675f5897-7ci7o|nginx] hello world\n2009-11-10T21:02:00.000000Z[namespace01|nginx-deployment-75675f5897-7ci7o|nginx] hello world\n2009-11-10T22:56:00.000000Z[namespace01|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:56:00.000000Z[namespace01|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:56:00.000000Z[namespace01|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:57:00.000000Z[namespace01|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:57:00.000000Z[namespace01|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:57:00.000000Z[namespace01|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:58:00.000000Z[namespace01|nginx-deployment-75675f5897-7ci7o|sidecar] hello from sidecar\n2009-11-10T22:58:00.000000Z[namespace01|nginx-deployment-75675f5897-7ci7o|sidecar] lerom from sidecar\n2009-11-10T22:58:00.000000Z[namespace01|nginx-deployment-75675f5897-7ci7o|sidecar] hello from sidecar\n2009-11-10T22:59:00.000000Z[namespace01|nginx-deployment-75675f5897-7ci7o|nginx] lerom ipsum\n2009-11-10T22:59:00.000000Z[namespace01|nginx-deployment-75675f5897-7ci7o|nginx] hello world"
-	assert.Equal(t, want, got)
-
-	got = execute("logs", "--query", `pod{namespace="namespace02"}`)
-	want = "2009-11-10T22:58:00.000000Z[namespace02|nginx-deployment-75675f5897-7ci7o|nginx] hello world\n2009-11-10T22:58:00.000000Z[namespace02|nginx-deployment-75675f5897-7ci7o|nginx] lerom ipsum\n2009-11-10T22:58:00.000000Z[namespace02|nginx-deployment-75675f5897-7ci7o|nginx] hello world\n2009-11-10T22:58:00.000000Z[namespace02|nginx-deployment-75675f5897-7ci7o|sidecar] hello from sidecar\n2009-11-10T22:58:00.000000Z[namespace02|nginx-deployment-75675f5897-7ci7o|sidecar] lerom from sidecar\n2009-11-10T22:58:00.000000Z[namespace02|nginx-deployment-75675f5897-7ci7o|sidecar] hello from sidecar\n2009-11-10T22:58:00.000000Z[namespace02|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:58:00.000000Z[namespace02|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:58:00.000000Z[namespace02|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:58:00.000000Z[namespace02|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:58:00.000000Z[namespace02|apache-75675f5897-7ci7o|httpd] hello from sidecar\n2009-11-10T22:58:00.000000Z[namespace02|apache-75675f5897-7ci7o|httpd] hello from sidecar"
-	assert.Equal(t, want, got)
-
+	for name, test := range tests {
+		t.Run(name, func(subt *testing.T) {
+			got := execute(test.args...)
+			assert.Equal(subt, test.want, got)
+		})
+	}
 	time.Sleep(3000 * time.Millisecond)
 }
