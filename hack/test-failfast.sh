7cb8846669567ea68406e9b866cb007c4f03b5b1
jmnote <opcore@gmail.com>
2023-04-16 17:09:02 +0000

    test-failfast

--- hack/test-failfast.sh
+++ hack/test-failfast.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+set -euo pipefail
 cd $(dirname $0)/../
 for s in $(go list ./...); do
     echo =============== $s ===============
-    go test -failfast -v $s || exit 1
+    go test -failfast -v $s
 done
