01aa33c0dda95eb128fb35950c56ee1b34d85a00
Jmnote <opcore@gmail.com>
2023-05-31 15:25:58 +0900

    init log_data_path (#85)
    * init log_data_path
    * v0.2.1 for bugfix

--- storage/fileservice/fileservice.go
+++ storage/fileservice/fileservice.go
@@ -2,6 +2,7 @@ package fileservice
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/kuoss/lethe/config"
 	storagedriver "github.com/kuoss/lethe/storage/driver"
@@ -17,6 +18,11 @@ func New(cfg *config.Config) (*FileService, error) {
 	driver, err := factory.Get("filesystem", map[string]interface{}{"RootDirectory": cfg.LogDataPath()})
 	if err != nil {
 		return nil, fmt.Errorf("factory.Get err: %w", err)
+	}
+	// TODO: use driver
+	err = os.MkdirAll(cfg.LogDataPath(), 0755)
+	if err != nil {
+		return nil, fmt.Errorf("mkdirAll err: %w", err)
 	}
 	return &FileService{cfg, driver}, nil
 }
--- storage/fileservice/fileservice_test.go
+++ storage/fileservice/fileservice_test.go
@@ -1,8 +1,10 @@
 package fileservice
 
 import (
+	"os"
 	"testing"
 
+	"github.com/kuoss/lethe/config"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,4 +12,29 @@ func TestNew(t *testing.T) {
 	assert.NotEmpty(t, fileService)
 	assert.Equal(t, "filesystem", fileService.driver.Name())
 	assert.Equal(t, "tmp/init", fileService.driver.RootDirectory())
+}
+
+func TestNewTemp(t *testing.T) {
+	cfg, err := config.New("test")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, cfg)
+
+	dataPath := "tmp/temp" // caution: RemoveAll()
+
+	cfg.SetLogDataPath(dataPath)
+	tempFileService, err := New(cfg)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, tempFileService)
+	assert.DirExists(t, dataPath) // exists
+
+	// duplicate ok
+	cfg.SetLogDataPath(dataPath)
+	tempFileService2, err := New(cfg)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, tempFileService2)
+	assert.DirExists(t, dataPath) // exists
+
+	// clean up
+	err = os.RemoveAll(dataPath)
+	assert.NoError(t, err)
 }
