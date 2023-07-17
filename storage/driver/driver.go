5f723d5dae151a95ec6ca351c6e7af452290a3ae
jmnote <opcore@gmail.com>
2023-07-17 12:41:32 +0000

    add comment & parameter name for driver interface

--- storage/driver/driver.go
+++ storage/driver/driver.go
@@ -4,18 +4,19 @@ import (
 	"io"
 )
 
+// https://github.com/distribution/distribution/blob/v2.8.2/registry/storage/driver/storagedriver.go#L41
 type Driver interface {
 	Name() string
-	GetContent(string) ([]byte, error)
-	PutContent(string, []byte) error
-	Reader(string) (io.ReadCloser, error)
-	Writer(string) (FileWriter, error)
-	Stat(string) (FileInfo, error)
-	List(string) ([]string, error)
-	Move(string, string) error
-	Delete(string) error
-	Walk(string) ([]FileInfo, error)
-	WalkDir(string) ([]string, error)
-	Mkdir(string) error
+	GetContent(path string) ([]byte, error)
+	PutContent(path string, content []byte) error
+	Reader(path string) (io.ReadCloser, error)
+	Writer(path string) (FileWriter, error)
+	Stat(path string) (FileInfo, error)
+	List(path string) ([]string, error)
+	Move(sourcePath string, destPath string) error
+	Delete(path string) error
+	Walk(path string) ([]FileInfo, error)
+	WalkDir(path string) ([]string, error)
+	Mkdir(path string) error
 	RootDirectory() string
 }
