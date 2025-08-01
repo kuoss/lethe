2308229e846ec96e382e41602f08d2da7d013e8b
jmnote <opcore@gmail.com>
2025-08-01 06:40:09 +0000

    toLetheQL

--- storage/queryservice/queryservice.go
+++ storage/queryservice/queryservice.go
@@ -2,7 +2,10 @@ package queryservice
 
 import (
 	"context"
+	"fmt"
 	"reflect"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/kuoss/common/logger"
@@ -31,6 +34,8 @@ func (s *QueryService) Query(ctx context.Context, qs string, tr model.TimeRange)
 			End:   now,
 		}
 	}
+
+	qs = toLetheQL(qs)
 	qry, err := s.engine.NewRangeQuery(qs, tr.Start, tr.End)
 	if err != nil {
 		return &letheql.Result{Err: err}
@@ -40,4 +45,55 @@ func (s *QueryService) Query(ctx context.Context, qs string, tr model.TimeRange)
 		logger.Errorf("exec err: %s", res.Err.Error())
 	}
 	return res
+}
+
+func toLetheQL(input string) string {
+	if !strings.HasPrefix(input, "{") {
+		return input
+	}
+
+	re := regexp.MustCompile(`^\{([^}]+)\}(.*)$`)
+	matches := re.FindStringSubmatch(input)
+
+	if len(matches) < 3 {
+		return input
+	}
+
+	contentInBraces := matches[1] // e.g., `job="pod",namespace="namespace01"``
+	remainderString := matches[2] // e.g., ` |= "hello world"`
+
+	// Regex to find and capture the 'job="VALUE"' part.
+	// This regex is made more robust to handle spaces around commas or at the start/end.
+	reType := regexp.MustCompile(`(?:^|\s*,\s*)job="([^"]+)"(?:$|\s*,\s*)`)
+	jobMatches := reType.FindStringSubmatch(contentInBraces)
+
+	// If no 'job' attribute is found, return the original input as per requirements.
+	if len(jobMatches) < 2 { // Changed from 3 to 2 as the new regex has only one capturing group for the value
+		return input
+	}
+
+	extractedType := jobMatches[1] // The captured VALUE of job (e.g., "pod", "node")
+
+	// Split by commas (with optional spaces) to process individual parts
+	parts := regexp.MustCompile(`\s*,\s*`).Split(contentInBraces, -1)
+
+	filteredParts := []string{}
+	jobRemoved := false
+	for _, part := range parts {
+		trimmedPart := strings.TrimSpace(part)
+		if !jobRemoved && strings.HasPrefix(trimmedPart, "job=") {
+			jobRemoved = true // Mark that 'job' has been processed/removed
+			continue
+		}
+		if trimmedPart != "" {
+			filteredParts = append(filteredParts, trimmedPart)
+		}
+	}
+
+	tempContent := strings.TrimSpace(strings.Join(filteredParts, ","))
+	if tempContent == "" {
+		return extractedType + remainderString
+	} else {
+		return fmt.Sprintf("%s{%s}%s", extractedType, tempContent, remainderString)
+	}
 }
--- storage/queryservice/queryservice_test.go
+++ storage/queryservice/queryservice_test.go
@@ -2,6 +2,7 @@ package queryservice
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/kuoss/common/tester"
@@ -22,6 +23,13 @@ func TestQuery(t *testing.T) {
 		wantError string
 		want      *letheql.Result
 	}{
+		// letheQL
+		{
+			`foo`,
+			model.TimeRange{},
+			"unknown logType: foo",
+			&letheql.Result{},
+		},
 		{
 			`pod`,
 			model.TimeRange{},
@@ -36,9 +44,24 @@ func TestQuery(t *testing.T) {
 				logmodel.PodLog{Time: "2009-11-10T22:59:00.000000Z", Namespace: "namespace01", Pod: "nginx-deployment-75675f5897-7ci7o", Container: "nginx", Log: "lorem ipsum"},
 				logmodel.PodLog{Time: "2009-11-10T22:59:00.000000Z", Namespace: "namespace01", Pod: "nginx-deployment-75675f5897-7ci7o", Container: "nginx", Log: "hello world"}}}, Warnings: model.Warnings(nil)},
 		},
+		// logQL
+		{
+			`{job="pod"}`,
+			model.TimeRange{},
+			"getTargets err: target matcher err: not found label 'namespace' for logType 'pod'",
+			&letheql.Result{},
+		},
+		{
+			`{job="pod",namespace="namespace01"}`,
+			model.TimeRange{},
+			"",
+			&letheql.Result{Err: error(nil), Value: model.Log{Name: "pod", Lines: []model.LogLine{
+				logmodel.PodLog{Time: "2009-11-10T22:59:00.000000Z", Namespace: "namespace01", Pod: "nginx-deployment-75675f5897-7ci7o", Container: "nginx", Log: "lorem ipsum"},
+				logmodel.PodLog{Time: "2009-11-10T22:59:00.000000Z", Namespace: "namespace01", Pod: "nginx-deployment-75675f5897-7ci7o", Container: "nginx", Log: "hello world"}}}, Warnings: model.Warnings(nil)},
+		},
 	}
 	for i, tc := range testCases {
-		t.Run(tester.CaseName(i), func(t *testing.T) {
+		t.Run(tester.CaseName(i, tc.qs), func(t *testing.T) {
 			clock.SetPlaygroundMode(true)
 			defer clock.SetPlaygroundMode(false)
 
@@ -63,6 +86,137 @@ func TestQuery(t *testing.T) {
 			}
 			res.Err = nil
 			require.Equal(t, tc.want, res)
+		})
+	}
+}
+
+// TestToLetheQL tests the toLetheQL function against various scenarios.
+func TestToLetheQL(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		// Standard cases
+		{
+			input: `{job="pod"}`,
+			want:  `pod`,
+		},
+		{
+			input: `{job="pod",namespace="namespace01"}`,
+			want:  `pod{namespace="namespace01"}`,
+		},
+		{
+			input: `{namespace="namespace01",job="pod"}`,
+			want:  `pod{namespace="namespace01"}`,
+		},
+		{
+			input: `{job="node"}`,
+			want:  `node`,
+		},
+		{
+			input: `{job="node",node="node01"}`,
+			want:  `node{node="node01"}`,
+		},
+		{
+			input: `{node="node01",job="node"}`,
+			want:  `node{node="node01"}`,
+		},
+		// Cases with additional string " |= "hello world""
+		{
+			input: `{job="pod"} |= "hello world"`,
+			want:  `pod |= "hello world"`,
+		},
+		{
+			input: `{job="pod",namespace="namespace01"} |= "hello world"`,
+			want:  `pod{namespace="namespace01"} |= "hello world"`,
+		},
+		{
+			input: `{namespace="namespace01",job="pod"} |= "hello world"`,
+			want:  `pod{namespace="namespace01"} |= "hello world"`,
+		},
+		{
+			input: `{job="node"} |= "hello world"`,
+			want:  `node |= "hello world"`,
+		},
+		{
+			input: `{job="node",node="node01"} |= "hello world"`,
+			want:  `node{node="node01"} |= "hello world"`,
+		},
+		{
+			input: `{node="node01",job="node"} |= "hello world"`,
+			want:  `node{node="node01"} |= "hello world"`,
+		},
+		// Edge cases and error handling
+		{
+			input: `just a string`, // Does not start with '{'
+			want:  `just a string`,
+		},
+		{
+			input: `some{job="test"}string`, // Does not start with '{'
+			want:  `some{job="test"}string`,
+		},
+		{
+			input: ``, // Empty input
+			want:  ``,
+		},
+		{
+			input: `{}`, // Empty braces, no job
+			want:  `{}`,
+		},
+		{
+			input: `{key="value"}`, // No job attribute
+			want:  `{key="value"}`,
+		},
+		{
+			input: `{job="test"}`, // Basic job
+			want:  `test`,
+		},
+		{
+			input: `{job="test",attr1="val1",attr2="val2"}`, // job at beginning
+			want:  `test{attr1="val1",attr2="val2"}`,
+		},
+		{
+			input: `{attr1="val1",job="test",attr2="val2"}`, // job in middle
+			want:  `test{attr1="val1",attr2="val2"}`,
+		},
+		{
+			input: `{attr1="val1",attr2="val2",job="test"}`, // job at end
+			want:  `test{attr1="val1",attr2="val2"}`,
+		},
+		{
+			input: `{job="test", }`, // job with trailing comma
+			want:  `test`,
+		},
+		{
+			input: `{ ,job="test"}`, // job with leading comma
+			want:  `test`,
+		},
+		{
+			input: `{ ,,job="test",,attr="value",, }`, // Many commas and spaces
+			want:  `test{attr="value"}`,
+		},
+		{
+			input: `{job="pod", namespace="namespace01" }`, // Spaces around attributes
+			want:  `pod{namespace="namespace01"}`,
+		},
+		{
+			input: `{job="pod" , namespace="namespace01"}`, // Spaces around commas
+			want:  `pod{namespace="namespace01"}`,
+		},
+		{
+			input: `{job="test"`, // Malformed: missing closing brace
+			want:  `{job="test"`,
+		},
+		{
+			input: `{attr="value",job="test`, // Malformed: missing closing quote for job
+			want:  `{attr="value",job="test`,
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(tester.CaseName(i, tc.input), func(t *testing.T) {
+			actual := toLetheQL(tc.input)
+			require.Equal(t, tc.want, actual, fmt.Sprintf("Input: %q", tc.input))
 		})
 	}
 }
